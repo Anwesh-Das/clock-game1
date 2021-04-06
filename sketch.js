97907f372b144727edd84e956303405b56771bfe
Anwesh-Das <74312099+Anwesh-Das@users.noreply.github.com>
2021-04-06 13:42:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+function setup() {
+  createCanvas(600,600);
+
+  angleMode(DEGREES);
+  
+}
+
+function draw() {
+  background("black");  
+  translate(250,250);
+  rotate(-90);
+
+  hr = hour();
+  mn = minute();
+  sc = second();
+
+  scAngle = map(sc,0,60,0,360);
+  mnAngle = map(mn,0,60,0,360);
+  hrAngle = map(hr%12,0,12,0,360);
+  
+
+  push();
+  rotate(scAngle);
+  stroke("blue");
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+
+  push();
+  rotate(mnAngle);
+  stroke("green");
+  strokeWeight(7);
+  line(0,0,70,0);
+  pop();
+
+  push();
+  rotate(hrAngle);
+  stroke("red");
+  strokeWeight(7);
+  line(0,0,50,0);
+  pop();
+  
+  drawSprites();
+}
